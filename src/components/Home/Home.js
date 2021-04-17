23ccb2f55bb1ba026ef839ff72fe9bfb1a9efea5
waleedkhaskhali <76614771+waleedkhaskhali@users.noreply.github.com>
2021-04-16 22:28:21 -0400

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,12 +6,16 @@ function Home() {
   return (
     <div>
       <section className="main">
-        <div className="adill">
-          <h1 className="egg">THE MOST BEAUTIFUL BIKES OUT THERE</h1>
-
-          <Link className="shop-button" to="/Products">
-            Shop Now
-          </Link>
+        <div className="container">
+          <h1 className="header">Simple.</h1>
+          <h2 className="header">Affordable.</h2>
+          <h3 className="header">Efficient.</h3>
+          <h4 className="header">Fast.</h4>
+          <div>
+            <Link className="shop-button" to="/Products">
+              Get It Now
+            </Link>
+          </div>
         </div>
       </section>
     </div>
